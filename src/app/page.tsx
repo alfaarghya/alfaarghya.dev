f782a4a78a59676ca714b89ba99b6f7c9bb39506
alfaarghya <alfaarghya.dev@gmail.com>
2024-10-03 19:09:28 +0530

    padding added 🥱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,7 @@ import Landing from "@/screens/Landing";
 
 const Home = () => {
   return (
-    // <main className="">
-    <main className="bg-[#0A0A0A] h-full">
+    <main className="bg-[#0A0A0A] h-full p-2">
       <Landing />
     </main>
   );
