4f0b71e53252df254604f6708ce87f99574aad4a
alfaarghya <alfaarghya.dev@gmail.com>
2024-10-04 11:30:29 +0530

    try to fix again

--- src/components/ui/AppBar.tsx
+++ src/components/ui/AppBar.tsx
@@ -14,7 +14,7 @@ const AppBar = () => {
   return (
     <div className="w-full flex justify-center items-center">
       <div
-        className={`mt-24 fixed  h-20 md:w-2/3 w-96 rounded-2xl bg-neutral-500/10 backdrop-blur-sm px-4 flex justify-between items-center z-50 transition-all duration-700 ease-in-out ${
+        className={`mt-24 fixed  h-20 md:w-2/3 sm:w-96 w-80 rounded-2xl bg-neutral-500/10 backdrop-blur-sm px-4 flex justify-between items-center z-50 transition-all duration-700 ease-in-out ${
           isVisible ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10"
         }`}
       >
@@ -27,13 +27,13 @@ const AppBar = () => {
             width={50}
             height={50}
             alt="alfaarghya logo"
-            className="md:h-14  md:w-14 "
+            className="md:h-14 h-10 md:w-14 w-10"
           />
           <span className=" text-[#D8E3EB] font-bold text-lg tracking-widest md:block hidden">
             alfaarghya.dev
           </span>
         </Link>
-        <div className="flex items-center gap-2  text-xs text-white/90 leading-none border border-white/30 px-4 py-2 rounded-full uppercase cursor-pointer">
+        <div className="flex items-center gap-2  text-xs text-white/90 leading-none border border-white/30 md:px-4 px-2 md:py-2 py-1 rounded-full md:uppercase lowercase cursor-pointer">
           <span className="relative w-2 h-2 rounded-full bg-green-500">
             <span className="absolute top-0 left-0 w-full h-full animate-ping rounded-full bg-green-500 opacity-75"></span>
           </span>
