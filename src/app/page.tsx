c50f698519bd7c92a4f6b34e28d643f22edf4917
alfaarghya <alfaarghya.dev@gmail.com>
2024-10-04 18:51:38 +0530

    fix: boot screen 💤

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import BootScreen from "@/screens/BootScreen";
 import Landing from "@/screens/Landing";
+import React from "react";
 
 const Home = () => {
   return (
-    <main className="bg-[#0A0A0A] h-full p-2">
+    <>
       <BootScreen />
-      <Landing />
-    </main>
+      <main className="bg-[#0A0A0A] h-full p-2">
+        <Landing />
+      </main>
+    </>
   );
 };
 
