f9836ffca157ee3d56fa383a25f4c20c5c85e002
alfaarghya <alfaarghya.dev@gmail.com>
2025-06-16 21:03:52 +0530

    some padding on Project page

--- src/components/ui/Projects.tsx
+++ src/components/ui/Projects.tsx
@@ -9,7 +9,7 @@ import { projects } from "@/stores/projectStore";
 const Projects = () => {
   const [modal, setModal] = useState<modal>({ active: false, index: 0 });
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-center justify-center pt-20">
       <div className="flex flex-col text-start items-start justify-center px-4 w-full md:w-[700px] lg:w-[1000px]">
         <div className="text-4xl w-full flex flex-col justify-center items-center md:font-extrabold font-bold text-white mt-10 py-4">
           <span className="text-[#b1b1b1]">Recent</span>
